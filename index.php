c8aaf65bfc1e41f3b45366bb93c72eaeed0de08d
staniszevskyy <expunge96@hotmail.com>
2018-10-23 07:59:37 +0200

    .

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+
+
+class Student {
+var $surname;
+var $name;
+function __construct($surname, $name){
+$this->surname = $surname;
+$this->name = $name;
+}
+function setSurname($surname){$this->surname = $surname;}
+function getName(){return $this->name;}
+
+
+
+?>
+</body>
+
+</html>
